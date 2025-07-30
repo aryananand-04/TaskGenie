a0fe4bb7e10c36b06764d6f9c39d8d3df39ccd02
aryananand-04 <aryananand.dev04@gmail.com>
2025-07-30 17:04:31 +0530

    Dummy Tests

---
+++ context/context_collector.py
@@ -0,0 +1,7 @@
+def collect_context() -> str:
+    """
+    Dummy function to simulate collecting context from past tasks/logs.
+    This will later be connected to actual JSON, database, or project logs.
+    """
+    # Return a placeholder string as if it's collected context
+    return "Bug previously reported in May 2025 related to ElasticSearch query format."
